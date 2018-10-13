35f0aa62cc8d707513ca03f013749e02f327f282
Tortera1999 <nikhilnarendra1999@gmail.com>
2018-10-13 02:55:03 -0400

    Added collection view and product model class

--- MHacks StockX/Model/CatVCCollectionCell.swift
+++ MHacks StockX/Model/CatVCCollectionCell.swift
@@ -17,7 +17,28 @@ class CatVCCollectionCell: UICollectionViewCell {
     
     var product: Product!{
         didSet{
-            
+            descriptionLabel.text = product.description!
+            currentbidLabel.text = product.currentBid!
+            downloadImage(from: URL(string: product.image!)!)
         }
     }
+    
+    func downloadImage(from url: URL) {
+        print("Download Started")
+        getData(from: url) { data, response, error in
+            guard let data = data, error == nil else { return }
+            print(response?.suggestedFilename ?? url.lastPathComponent)
+            print("Download Finished")
+            DispatchQueue.main.async() {
+                self.image.image = UIImage(data: data)
+            }
+        }
+    }
+    
+    func getData(from url: URL, completion: @escaping (Data?, URLResponse?, Error?) -> ()) {
+        URLSession.shared.dataTask(with: url, completionHandler: completion).resume()
+    }
 }
+
+
+
