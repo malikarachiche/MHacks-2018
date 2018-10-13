c097525f0abce79ce861e59f9050224e103e29ce
Malik Arachiche <arachich@msu.edu>
2018-10-12 22:00:31 -0400

    Start of project

---
+++ MHacks StockX/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MHacks StockX
+//
+//  Created by Malik Arachiche on 10/12/18.
+//  Copyright © 2018 Malik Arachiche. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
