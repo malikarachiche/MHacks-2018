c87be48e5736a0dfb625ff11db01fc0747683a7e
Swapnik Katkoori <katkoor2@msu.edu>
2018-10-13 01:15:18 -0400

    init

--- MHacks StockX/ViewController.swift
+++ MHacks StockX/ViewController.swift
@@ -9,9 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var arr = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
