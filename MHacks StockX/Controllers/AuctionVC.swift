b9792a00d81c4b1111f2583af2b37157789e6a81
Tortera1999 <nikhilnarendra1999@gmail.com>
2018-10-13 12:52:32 -0400

    Started progress view

--- MHacks StockX/Controllers/AuctionVC.swift
+++ MHacks StockX/Controllers/AuctionVC.swift
@@ -16,17 +16,28 @@ class AuctionVC: UIViewController {
     
     @IBOutlet weak var initialPriceLabel: UILabel!
     
+    @IBOutlet weak var circProgressIndi: CircularProgressIndicator!
+    
     @IBOutlet weak var currentBidLabel: UILabel!
     
+    @IBOutlet weak var countdownLbl: UILabel!
     @IBOutlet weak var bidTextField: UITextField!
+    
+    var firstAppearance: Date?
+    
+    var config: Config!
     
     var product: Product!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        countdownLbl.font = UIFont.monospacedDigitSystemFont(ofSize: 120.0, weight: UIFont.Weight.thin)
+        
         productDescriptionLabel.text = product.description!
         initialPriceLabel.text = "The current bid is " + product.currentBid!
+        config = Config(startDate: Date(timeIntervalSince1970: product.ti), endDate: <#T##Date#>)
         downloadImage(from: URL(string: product.image!)!)
-        // Do any additional setup after loading the view.
+        // Do any additional setup after loading the .
     }
     
     @IBAction func bidNow(_ sender: Any) {
