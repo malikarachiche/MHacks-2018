42707941ca7ec8efd3d93521fb381abf3fddffba
Sehajbir Randhawa <srandhaw@purdue.edu>
2018-10-14 06:24:41 -0400

    UI improvements

---
+++ MHacks StockX/View/RoundedButton.swift
@@ -0,0 +1,17 @@
+//
+//  RoundedButton.swift
+//  MHacks StockX
+//
+//  Created by Sehajbir Randhawa on 10/14/18.
+//  Copyright © 2018 Malik Arachiche. All rights reserved.
+//
+
+import UIKit
+
+class RoundedButton: UIButton {
+
+    override func awakeFromNib() {
+        self.layer.cornerRadius = 5
+    }
+
+}
