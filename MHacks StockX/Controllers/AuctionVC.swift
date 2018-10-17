c1b5ff47ad63f42217517c659fcc306720f9186c
Sehajbir Randhawa <srandhaw@purdue.edu>
2018-10-17 03:25:35 -0400

    improvements in auction but there are some failing edge cases

--- MHacks StockX/Controllers/AuctionVC.swift
+++ MHacks StockX/Controllers/AuctionVC.swift
@@ -43,118 +43,143 @@ class AuctionVC: UIViewController {
     
     var maxProdAmount = "0.0"
     
-    override func viewWillAppear(_ animated: Bool) {
-        guard let uuid = product.uuid else {
-            //print("vfvgdf")
-            return
-        }
-        self.DB_BASE.child("Products").child(uuid).child("Bids").observe(.childAdded) { (snapshot) in
-
-           // self.messageDisplay(message: "A new bid of $\(self.DB_BASE.child("Products").child(uuid).child("HighestBid").value(forKey: "Amount") as! String) has been set")
-
-
-            guard let time = snapshot.childSnapshot(forPath: "Time").value as? String else{
-                return
-            }
-
-            guard let amount = snapshot.childSnapshot(forPath: "Amount").value as? String else{
-                return
-            }
-            
-            
-
-            if(amount != nil || amount != "") {
-                self.currentBidLabel.text = "\(amount)"
-                self.messageDisplay(message: "A new bid of $\(amount) has been set")
-            } else {
-                self.currentBidLabel.text = "0"
-            }
-
-            let myFloat = (time as NSString).doubleValue
-            let diff = Date().timeIntervalSince1970 - myFloat
-            //print(diff)
-            self.fdiff = 120 - diff
-
-
-
-            if(self.fdiff > 1 && self.fdiff <= 120){
-                self.progressBar.startProgress(to: 0, duration: 0, completion: {
-                    self.progressBar.startProgress(to: 100, duration: self.fdiff, completion: {
-                        
-                       //  self.currentBidLabel.text = "0"
-                        self.dismiss(animated: true, completion: nil)
-                        self.messageDisplay(message: "The auction is now over")
-                        self.DB_BASE.child("Products").child(uuid).removeValue()
-                     //   self.initialPriceLabel.text = "This Item is auctioned!"
-                        self.bidBtn.isEnabled = false
-                        
-                    })
-                })
-
-            }
-            else{
-                self.DB_BASE.child("Products").child(uuid).removeValue()
-
-            }
-
-
-
-
-
-        }
-        
-       
-        
-        
-    }
+//    override func viewWillAppear(_ animated: Bool) {
+//        
+//        print("\n\nIn will appear\n\n")
+//        guard let uuid = product.uuid else {
+//            return
+//        }
+//        self.DB_BASE.child("Products").child(uuid).child("HigestBid").observeSingleEvent(of: .value) { (snapshot) in
+//            
+//            
+//            guard let time = snapshot.childSnapshot(forPath: "Time").value as? String else{
+//                return
+//            }
+//            
+//            guard let amount = snapshot.childSnapshot(forPath: "Amount").value as? String else{
+//                return
+//            }
+//            
+//            
+//            
+//            if(amount != nil || amount != "") {
+//                self.currentBidLabel.text = "\(amount)"
+//                
+//            } else {
+//                self.currentBidLabel.text = "0"
+//            }
+//            
+//            let myFloat = (time as NSString).doubleValue
+//            let diff = Date().timeIntervalSince1970 - myFloat
+//            self.fdiff = 60 - diff
+//            
+//            
+//            
+//            if(self.fdiff > 1 && self.fdiff <= 60){
+//                
+//                
+//                self.progressBar.startProgress(to: 0, duration: 1, completion: {
+//                    
+//                    
+//                    self.progressBar.startProgress(to: 100, duration: self.fdiff, completion: {
+//                        
+//                        
+//                        self.dismiss(animated: true, completion: nil)
+//                        self.messageDisplay(message: "The auction is now over")
+//                        self.DB_BASE.child("Products").child(uuid).removeValue()
+//                        
+//                        
+//                    })
+//                })
+//                
+//            }
+//            else{
+//                self.DB_BASE.child("Products").child(uuid).removeValue()
+//                
+//            }
+//            
+//            
+//            
+//            
+//            
+//        }
+//        
+//        
+//        
+//        
+//    }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(true)
+        
         guard let uuid = product.uuid else {
-           // print("vfvgdf")
             return
         }
         
-    
         
-
-        DB_BASE.child("Products").child(uuid).child("HighestBid").observeSingleEvent(of: .value) { (snapshot) in
-
+        
+        self.DB_BASE.child("Products").child(product.uuid!).child("Bids").observe(.childAdded)  { (snapshot) in
+            
+            
             guard let time = snapshot.childSnapshot(forPath: "Time").value as? String else{
                 return
             }
-
+            
             guard let amount = snapshot.childSnapshot(forPath: "Amount").value as? String else{
                 return
             }
-
+            
+            
+            
+            
             if(amount != nil || amount != "") {
                 self.currentBidLabel.text = "\(amount)"
+                self.messageDisplayNoAction(message: "A new bid of $\(amount) has been set")
+                
             } else {
                 self.currentBidLabel.text = "0"
             }
-
+            
+            
             let myFloat = (time as NSString).doubleValue
             let diff = Date().timeIntervalSince1970 - myFloat
-            //print(diff)
-            self.fdiff = 120 - diff
+            self.fdiff = 60 - diff
             
             
-
-            if(self.fdiff > 1 && self.fdiff <= 120){
-               // self.progressBar.startProgress(to: 0, duration: 0)
-                self.progressBar.startProgress(to: 100, duration: self.fdiff)
-
+            
+            if(self.fdiff > 1 && self.fdiff <= 60){
+                
+                
+                self.progressBar.startProgress(to: 0, duration: 1, completion: {
+                    
+                    
+                    self.progressBar.startProgress(to: 100, duration: self.fdiff, completion: {
+                        
+                        
+                        self.dismiss(animated: true, completion: nil)
+                        self.messageDisplay(message: "The auction is now over")
+                        self.DB_BASE.child("Products").child(uuid).removeValue()
+                        
+                        
+                    })
+                })
+                
+                
+                
+                
             }
             else{
                 self.DB_BASE.child("Products").child(uuid).removeValue()
-
+                
             }
-
-
-
-
+            
+            
+            
+            
+            
         }
+        
+        
         
     }
     
@@ -163,12 +188,12 @@ class AuctionVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        
         productDescriptionLabel.text = product.description!
         initialPriceLabel.text = "The starting price is $\(product.currentBid!)"
         currentBidLabel.text = "0"
         self.downloadImage(from: URL(string: self.product.image!)!)
         
-      
         
         
         
@@ -178,7 +203,6 @@ class AuctionVC: UIViewController {
         }, product: product)
         
         
-        
     }
     
     
@@ -186,44 +210,10 @@ class AuctionVC: UIViewController {
     
     @IBAction func bidNow(_ sender: Any) {
         
-        guard let uuid = product.uuid else {
-            return
-        }
-        //print(currentBidLabel.text!)
         DataService.instance.bid(product: product, amount: bidTextField.text!) { (success, tempTime) in
-            if(success) {
-               // self.messageDisplay(message: "A new bid of $\(self.bidTextField.text!) has been set")
-                self.currentBidLabel.text = self.bidTextField.text!
-
-                
-                print("You on the right track")
-
-                let myFloat = (tempTime as NSString).doubleValue
-                let diff = Date().timeIntervalSince1970 - myFloat
-                
-                self.fdiff = 120 - diff
-                if(self.fdiff > 1 && self.fdiff<=120){
-                    self.progressBar.startProgress(to: 0, duration: 0, completion: {
-                        self.progressBar.startProgress(to: 100, duration: self.fdiff, completion: {
-                            self.dismiss(animated: true, completion: nil)
-                            self.specMessage(message: "The auction is now over")
-                            
-                             self.DB_BASE.child("Products").child(uuid).removeValue()
-                        })
-                    })
-                    
-
-//                } else{
-//                    self.DB_BASE.child("Products").child(uuid).removeValue()
-//                }
-                }
-            } else
-            {
-                //print("False, alert msg")
-                self.alertcontrollerDisplay(message: "Error. Enter a higher bid.")
-            }
+            
         }
-    
+        
     }
     
     func alertcontrollerDisplay(message: String){
@@ -237,7 +227,16 @@ class AuctionVC: UIViewController {
     func messageDisplay(message: String){
         let alertController = UIAlertController(title: "Message", message: message, preferredStyle: .alert)
         let okAction = UIAlertAction(title: "Ok", style: .default) { (action) in
-        
+            self.dismiss(animated: true, completion: nil)
+        }
+        alertController.addAction(okAction)
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
+    func messageDisplayNoAction(message: String){
+        let alertController = UIAlertController(title: "Message", message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "Ok", style: .default) { (action) in
+            
         }
         alertController.addAction(okAction)
         self.present(alertController, animated: true, completion: nil)
@@ -252,7 +251,7 @@ class AuctionVC: UIViewController {
         alertController.addAction(UIAlertAction(title: "OK", style: .default, handler: { (aa) in
             self.dismiss(animated: true, completion: nil)
         }))
-       
+        
         
         self.present(alertController, animated: true, completion: nil)
     }
@@ -275,11 +274,11 @@ class AuctionVC: UIViewController {
         }
     }
     
-        
-        func getData(from url: URL, completion: @escaping (Data?, URLResponse?, Error?) -> ()) {
-            URLSession.shared.dataTask(with: url, completionHandler: completion).resume()
-        }
-        
     
-        
+    func getData(from url: URL, completion: @escaping (Data?, URLResponse?, Error?) -> ()) {
+        URLSession.shared.dataTask(with: url, completionHandler: completion).resume()
+    }
+    
+    
+    
 }
